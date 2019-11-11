243669d7ab435e7e24d76089750dd9fd785a162a
Solaiman Jawad <solaimanjawad@gmail.com>
2019-11-10 23:11:25 -0500

    today's progress. Planning to create most of the backend b4 trip to BD

--- maze.cc
+++ maze.cc
@@ -1,11 +1,17 @@
 #include "maze.h"
+#include <vector>
 
-Cell::Cell(Point <int> & other): centerSquare{new Point<int>{other}} {}
+Cell::Cell(Point <int> & other): centerSquare{other} {}
 
-Cell::Cell(const Cell & other): centerSquare{new Point<int>{*(other.centerSquare)}} {}
+Cell::Cell(int x, int y): centerSquare{x,y} {}
 
-double Cell::operator=(const Cell & other) {
-
-
+double Cell::judgeDistance(Cell & other) {
+    return centerSquare.EucledianDistance(other.centerSquare);
 }
+
+Grid::Grid(int size): theGrid{std::vector<std::vector<int>>
+    (size,std::vector<int>(size,0))} {}
+
+Grid::Grid(int sizex, int sizey): theGrid{std::vector<std::vector<int>> (sizey,std::vector<int>(sizex,0))} {}
+
 
--- maze.h
+++ maze.h
@@ -5,7 +5,7 @@
 #include <ostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 
 
 template <typename T> class Point {
@@ -13,15 +13,21 @@ template <typename T> class Point {
         T y_coord;
 
     public:
-        Point(T x, T y): x_coord{x}, y_coord{y} {};
+        Point(T x, T y): x_coord{x}, y_coord{y} {}
         Point(Point <T> & other): x_coord{other.x_coord}, y_coord{other.x_coord} {}
-        ~Point() {};
+        ~Point() {}
+        
+        std::pair<T, T> getcoords() {
+            std::pair<T,T> retval = std::pair<T,T>{x_coord, y_coord};
+            return retval;
+        }
+
         Point <T> operator + (Point <T> & other) {
             Point <T> temp = Point<T>{x_coord, y_coord};
             temp.x_coord = temp.x_coord + other.x_coord;
             temp.y_coord = temp.y_coord + other.y_coord;
             return temp;
-        };
+        }
         Point <T> operator - (Point <T> & other) {
             Point <T> temp = Point<T>{x_coord, y_coord};
             temp.x_coord = temp.x_coord - other.x_coord;
@@ -37,17 +43,20 @@ template <typename T> class Point {
 };
 
 class Cell {
-    Point <int> * centerSquare;
+    Point <int> centerSquare;
 
     Cell(Point<int> & other);
-    Cell(const Cell & other);
-    double judgeDistance(Cell &);
+    Cell(int x, int y);
+    double judgeDistance(Cell & other);
     ~Cell();
 };
 
 
 class Grid {
+
+    std::vector<std::vector<int>> theGrid;
     Grid(int size);
+    Grid(int sizex, int sizey);
     void GenerateMaze();
     void ColourCell(Point <int> cell,std::string colour);
     void GetCell(Point <int> cell);
